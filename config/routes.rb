4015cddda5d9596bb5795480688e38321456208e
Aaron Boakye <cheminko@hotmail.com>
2024-10-11 15:04:37 +0100

    authentication + sign up setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,14 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/destroy'
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/create'
-  get 'bookings/destroy'
-  get 'garments/index'
-  get 'garments/show'
-  get 'garments/new'
-  get 'garments/create'
-  get 'garments/edit'
-  get 'garments/update'
-  get 'garments/destroy'
-  devise_for :users
-  root "garments#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  
+  resources :garments
+  resources :bookings, except: [:edit, :update]
+  resources :reviews, except: [:edit, :update]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Garment.destroy_all
+Booking.destroy_all
+
