79b709e864cd07322ee2a60f249ade0b4379f7bd
Aaron Boakye <cheminko@hotmail.com>
2024-10-18 20:30:06 +0100

    navbar 80%

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_12_190613) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_18_170521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_12_190613) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "garments", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -63,6 +69,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_12_190613) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.string "size"
     t.index ["user_id"], name: "index_garments_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,62 +22,89 @@ users.each do |user_data|
   puts "User #{user_data[:username]} created!"
 end
 
+
+puts "Seeding categories..."
+
+categories = {
+  "Men's Leather Jackets" => Category.create!(name: "Men's Leather Jackets"),
+  "Dresses" => Category.create!(name: "Dresses"),
+  "Sneakers" => Category.create!(name: "Sneakers"),
+  "Men's Sweaters" => Category.create!(name: "Men's Sweaters"),
+  "Jeans" => Category.create!(name: "Jeans"),
+  "Bomber Jackets" => Category.create!(name: "Bomber Jackets"),
+  "Sunglasses" => Category.create!(name: "Sunglasses"),
+  "Women's Boots" => Category.create!(name: "Women's Boots"),
+  "Women's coats" => Category.create!(name: "Women's coats")
+}
+
+puts "Categories seeded"
+
 puts "Seeding garments..."
 
 garments = [
-  { title: "Avirex Varsity Jacket", description: "Vintage Avirex. An iconic jacket worn by many East Coast Hip Hop pioneers. Very rare. Size M", category: "Men's Leather Jackets", brand: "Avirex", rental_price: 200, photos: [
+  { title: "Avirex Varsity Jacket", description: "Vintage Avirex. An iconic jacket worn by many East Coast Hip Hop pioneers. Very rare.", category_name: "Men's Leather Jackets", brand: "Avirex", rental_price: 200, size: "M", photos: [
     Rails.root.join("app/assets/images/AVIREX 1.webp"),
     Rails.root.join("app/assets/images/AVIREX 2.webp"),
     Rails.root.join("app/assets/images/AVIREX 3.webp"),
     Rails.root.join("app/assets/images/AVIREX 4.webp")
   ]},
-  { title: "Dior Belted Red Dress", description: "Belted mid-length dress, a hallmark Dior silhouette, embodies modern elegance. Size L", category: "Dresses", brand: "Dior", rental_price: 350, photos: [
+  { title: "Dior Belted Red Dress", description: "Belted mid-length dress, a hallmark Dior silhouette, embodies modern elegance.", category_name: "Dresses", brand: "Dior", rental_price: 350, size: "L", photos: [
     Rails.root.join("app/assets/images/Dior Belted Mid-Length Dress.webp"),
   ]},
-  { title: "Nike Air Mags", description: "Back To The Future Nike Air Mags with adaptive fit technology and power laces. One of a kind design. Size 9 EU", category: "Sneakers", brand: "Nike", rental_price: 500, photos: [
+  { title: "Nike Air Mags", description: "Back To The Future Nike Air Mags with adaptive fit technology and power laces. One of a kind design.", category_name: "Sneakers", brand: "Nike", rental_price: 500, size: "9 EU", photos: [
     Rails.root.join("app/assets/images/Nike MAG Back to The Future .webp"),
     Rails.root.join("app/assets/images/Nike MAG Back To The Future 2.webp"),
     Rails.root.join("app/assets/images/Nike MAG Back To The Future 3.webp"),
     Rails.root.join("app/assets/images/Nike MAG Back To The Future 4.webp")
   ]},
-  { title: "Axel Arigato Cardigan", description: "Cardigan knitted from contrasting wool-blend panels featuring exposed seams. Hand-embroidery finishes this piece. Size L", category: "Men's Sweaters", brand: "Axel Arigato", rental_price: 100, photos: [
+  { title: "Axel Arigato Cardigan", description: "Cardigan knitted from contrasting wool-blend panels featuring exposed seams. Hand-embroidery finishes this piece.", category_name: "Men's Sweaters", brand: "Axel Arigato", rental_price: 100, size: "L", photos: [
     Rails.root.join("app/assets/images/Axel Arigato Cardigan.jpg"),
   ]},
-  { title: "Coogi Sweater", description: "The signature COOGI Brights sweater executed in the vibrant colors of Fall - bold and expressive. Unmistakably COOGI. Size S", category: "Men's Sweaters", brand: "COOGI", rental_price: 250, photos: [
+  { title: "Coogi Sweater", description: "The signature COOGI Brights sweater executed in the vibrant colors of Fall - bold and expressive. Unmistakably COOGI.", category_name: "Men's Sweaters", brand: "COOGI", rental_price: 250, size: "S", photos: [
     Rails.root.join("app/assets/images/Coogi sweater.webp"),
     Rails.root.join("app/assets/images/COOGI 2.webp"),
     Rails.root.join("app/assets/images/COOGI 3.webp"),
     Rails.root.join("app/assets/images/COOGI 4.webp")
   ]},
-  { title: "Evisu Jeans", description: "Mid-rise fit, slim-leg cut, belt loops, five pockets, contrast stitching, designer plaque at back, logo print at back with metallic embroidery. Size W32", category: "Jeans", brand: "Evisu", rental_price: 100, photos: [
+  { title: "Evisu Jeans", description: "Mid-rise fit, slim-leg cut, belt loops, five pockets, contrast stitching, designer plaque at back, logo print at back with metallic embroidery.", category_name: "Jeans", brand: "Evisu", rental_price: 100, size: "W32", photos: [
     Rails.root.join("app/assets/images/Evisu 1.jpg"),
     Rails.root.join("app/assets/images/Evisu 2.jpg"),
     Rails.root.join("app/assets/images/evisu 3.jpg"),
     Rails.root.join("app/assets/images/Evisu 4.jpg")
   ]},
-  { title: "Kapital Bomber Jacket", description: "Padded bomber jacket for man by Kapital in satin khaki with orange detail on the left side. This jacket can be transformed into a soft pillow! Size M", category: "Bomber Jackets", brand: "Kapital", rental_price: 300, photos: [
+  { title: "Kapital Bomber Jacket", description: "Padded bomber jacket for man by Kapital in satin khaki with orange detail on the left side. This jacket can be transformed into a soft pillow!", category_name: "Bomber Jackets", brand: "Kapital", rental_price: 300, size: "M", photos: [
     Rails.root.join("app/assets/images/KAPITAL 1.jpg"),
     Rails.root.join("app/assets/images/KAPITAL 2.jpg"),
     Rails.root.join("app/assets/images/KAPITAL 3.jpg"),
     Rails.root.join("app/assets/images/KAPITAL 4.jpg")
   ]},
-  { title: "Gentle Monster Sunglasses", description: "a simple black frame and a contemporary rectangular silhouette, this model can be paired with any look", category: "Sunglasses", brand: "Gentle Monster", rental_price: 50, photos: [
+  { title: "Gentle Monster Sunglasses", description: "A simple black frame and a contemporary rectangular silhouette, this model can be paired with any look.", category_name: "Sunglasses", brand: "Gentle Monster", rental_price: 50, size: nil, photos: [
     Rails.root.join("app/assets/images/GENTLE MONSTER.avif")
   ]},
-  { title: "Maison Margiela Tabi Ankle Boots", description: "Tabi 60mm leather ankle boots in black with signature Tabi toe", category: "Women's Boots", brand: "Gentle Monster", rental_price: 50, photos: [
+  { title: "Maison Margiela Tabi Ankle Boots", description: "Tabi 60mm leather ankle boots in black with signature Tabi toe.", category_name: "Women's Boots", brand: "Maison Margiela", rental_price: 50, size: "5", photos: [
     Rails.root.join("app/assets/images/TABI BOOTS.jpg"),
     Rails.root.join("app/assets/images/TABI BOOTS 2.webp"),
     Rails.root.join("app/assets/images/TABI BOOTS 3.webp"),
     Rails.root.join("app/assets/images/TABI BOOTS 4.webp")
   ]},
-  { title: "Burberry Trench Coat", description: "The iconic Burberry trench coat, made in England from shower-resistant cotton gabardine. UK Size 10", category: "Women's coats", brand: "Burberry", rental_price: 200, photos: [
+  { title: "Burberry Trench Coat", description: "The iconic Burberry trench coat, made in England from shower-resistant cotton gabardine.", category_name: "Women's coats", brand: "Burberry", rental_price: 200, size: "12", photos: [
     Rails.root.join("app/assets/images/Burberry 1.webp")
   ]}
 ]
 
 garments.each do |data|
-  garment = Garment.create!(title: data[:title],
-  description: data[:description], category: data[:category], brand: data[:brand], rental_price: data[:rental_price], user_id: User.pluck(:id).sample)
+  # Find the category by name
+  category = Category.find_by(name: data[:category_name])
+
+  garment = Garment.create!(
+    title: data[:title],
+    description: data[:description],
+    category: category,
+    brand: data[:brand],
+    rental_price: data[:rental_price],
+    size: data[:size], # Add size here
+    user_id: User.pluck(:id).sample
+  )
 
   if data[:photos]
     data[:photos].each do |photo_path|
