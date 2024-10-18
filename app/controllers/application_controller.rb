084835a437c421df4646efc4d73d738e401d3830
Aaron Boakye <cheminko@hotmail.com>
2024-10-18 20:21:10 +0100

    navbar 80%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::Base
   # app/controllers/application_controller.rb
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_categories
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+
+  def set_categories
+    @categories = Category.all
   end
 end
