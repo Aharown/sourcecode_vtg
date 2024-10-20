f8b283e82db5ce43432499abd20c780230beb78e
Aaron Boakye <cheminko@hotmail.com>
2024-10-20 20:56:53 +0100

    index page completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         has_one_attached :profile_photo
          has_many :bookings
          has_many :garments, dependent: :nullify
 end
