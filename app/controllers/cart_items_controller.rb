85e6fa6e690ec8da0a586bc72abc5df8c8f2bf51
Aaron Boakye <cheminko@hotmail.com>
2025-06-13 01:02:29 +0100

    stimulus js added for add to cart button, revealing continue shopping and view cart buttons

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,13 +2,12 @@ class CartItemsController < ApplicationController
   def create
     id = params[:item_id].to_s
     session[:cart] ||= {}
+    session[:cart][id] ||= 1
 
-    if session[:cart].key?(id)
-    else
-      session[:cart][id] = 1
+    respond_to do |format|
+      format.html { redirect_to cart_path }
+      format.json { render json: { added: true } }
     end
-
-    redirect_to cart_path
   end
 
   def update
