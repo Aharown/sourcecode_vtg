d6e4b3b7981af841f041ef1a153cceb2ab782708
Aaron Boakye <cheminko@hotmail.com>
2025-08-06 23:30:04 +0100

    admin dashboard access fix for production

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_one_attached :profile_photo
-         has_many :bookings
-         has_many :garments, dependent: :nullify
+
+  has_one_attached :profile_photo
+  has_many :bookings
+  has_many :garments, dependent: :nullify
+
   attribute :admin, :boolean, default: false
 
   def admin?
     self.admin
   end
-
 end
